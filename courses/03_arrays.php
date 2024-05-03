9290b779eaee6ccf8980a635a8090cc6ceb783fb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-03 14:43:21 +0200

    03 arrays: update + formatting

--- courses/03_arrays.php
+++ courses/03_arrays.php
@@ -1,6 +1,6 @@
 <?php
 
-/* ----------- Arrrays ----------- */
+/* ----------- Arrays ----------- */
 
 /* If you need to store multiple values, 
 you can use arrays. Arrays hold "elements" */
@@ -15,6 +15,9 @@ $numbers_2 = array(1, 2, 3, 4, 5);
 $colors = ['red', 'blue', 'green'];
 $colors2 = array('red', 'blue', 'green');
 
+// mixed values array
+$mixed = [True, "one", 1];
+var_dump($mixed);
 
 // Outputting values from an array
 echo $numbers[0];
@@ -34,8 +37,7 @@ echo "</pre>";
 
 /* ------ Associative Arrays ----- */
 
-/* Associative arrays allow us to use named keys to id
-entify values.*/
+/* Associative arrays allow us to use named keys to identify its values.*/
 
 $colors = [
   1 => 'red',
@@ -66,7 +68,7 @@ echo '<br/>----------------------------------------<br/>';
 
 /* ---- Multi-dimensional arrays ---- */
 
-/* Multi-dimansional arrays are often used to 
+/* Multi-dimensional arrays are often used to 
 store data in a table format.*/
 
 // Single person
@@ -76,9 +78,16 @@ $person1 = [
   'email' => 'brad@gmail.com',
 ];
 
+$person2 = [
+  'first_name' => 'Mike',
+  'last_name' => 'Tyson',
+  'email' => 'm__tyson@gmail.com',
+];
+
 // Array of people
 $people = [
-  $person1, //   [...$person1]
+  $person1,
+  [...$person2],
   [
     'first_name' => 'John',
     'last_name' => 'Doe',
@@ -107,8 +116,8 @@ print_r(json_encode($people));
 echo "</pre>";
 
 // Decode from JSON
-$jsonobj = '{"first_name":"Brad","last_name": "Traversy","email":"brad@gmail.com"}';
+$jsonObject = '{"first_name":"Brad","last_name": "Traversy","email":"brad@gmail.com"}';
 
 echo "<pre>";
-print_r(json_decode($jsonobj));
+print_r(json_decode($jsonObject));
 echo "</pre>";
