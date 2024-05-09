304cbb07d93bac56265fd00278427226f5b8ffbe
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-09 21:23:31 +0200

    11 sanitizing input: updated

--- courses/11_sanitizing_inputs.php
+++ courses/11_sanitizing_inputs.php
@@ -1,41 +1,32 @@
 <?php
 /* --- Sanitizing Inputs -- */
 
+echo "\$_SERVER['PHP_SELF'] = ", $_SERVER['PHP_SELF'];
+
 /*
 Data submitted through a form is not sanitized 
 by default. We have methods to sanitize data manually.
 */
 
-/* 1 - No Sanitize: DANGER !--------------------------------------------*/
 if (isset($_POST['name']) && isset($_POST['email'])) {
 
+  /* 1 - No Sanitize: DANGER !--------------------------------------------*/
   $name = $_POST['name'];
   $email = $_POST['email'];
-}
 
-
-/* 2 - htmlspecialchars() - Convert special characters to HTML entities---*/
-if (isset($_POST['name']) && isset($_POST['email'])) {
-
+  /* 2 - htmlspecialchars() - Convert special characters to HTML entities---*/
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
-}
 
-
-/* 3 - filter_var() - Sanitize data   -------------------------------------*/
-if (isset($_POST['name']) && isset($_POST['email'])) {
-
+  /* 3 - filter_var() - Sanitize data   -------------------------------------*/
   $name = filter_var($_POST['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-}
 
-
-/* 4 - filter_input() - Sanitize inputs ----------------------------------- */
-if (isset($_POST['name']) && isset($_POST['email'])) {
-
+  /* 4 - filter_input() - Sanitize inputs ----------------------------------- */
   $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 }
+
 
 ?>
 
