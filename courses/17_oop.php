71d09cabd43f3b6d5a5fd90b47181d45b4bfe281
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-23 10:30:52 +0200

    17 OOP updated

--- courses/17_oop.php
+++ courses/17_oop.php
@@ -14,30 +14,39 @@
 
 class User
 {
-  /* Properties are just variables that belong to a class.
+  /* 
+   Properties are attribute that belong to a class.
+   
    Access Modifiers: public, private, protected
    public    - can be accessed from anywhere
    private   - can only be accessed from inside the class
-   protected - can only be accessed from inside the class and by inheriting classes */
+   protected - can only be accessed from inside the class and by inheriting classes 
+  
+  */
 
   private $name;
   public $email;
   public $password;
 
-  /* The constructor is called whenever an object is created from the class.
-   We pass in the properties to the constructor from the outside. */
+  /* 
+  The constructor is called whenever an object is created 
+  from the class. We pass in the properties to the constructor 
+  from the outside. 
+  */
 
   public function __construct($name, $email, $password)
   {
-    /* We assign the properties passed in from the outside 
-    to the properties we created inside the class. */
+    /* 
+    We assign the properties passed in from the outside 
+    to the properties we created inside the class. 
+    */
 
     $this->name = $name;
     $this->email = $email;
     $this->password = $password;
   }
 
-  /* Methods are functions that belong to a class.*/
+  # Methods are functions that belong to a class.
 
   // function setName($name)
   // {
@@ -57,7 +66,7 @@ class User
   // Destructor is called when an object is destroyed or the end of the script.
   function __destruct()
   {
-    echo "The user name is {$this->name} <br />";
+    echo "<br>" . "FROM __destruct :" . "<br/>" . "The user name is {$this->name} <br />";
   }
 }
 
@@ -72,20 +81,23 @@ echo $user1->login(), '<br />';
 var_dump($user1);
 // echo $user1->name;
 
-echo '<br />';
+echo '<br/>---------------------------------------------------<br/>';
 
 
-
-/* Inheritence 
+/* Inheritance 
 ================ */
 
 /*
-  Inheritence is the ability to create a new class from an existing class.
-  It is achieved by creating a new class that 'extends' an existing class.
+  Inheritance is the ability to create a new class 
+  from an existing class.
+  
+  It is achieved by creating a new class that 'extends' 
+  an existing class.
 */
 
 class Employee extends User
 {
+  public string $title;
   public function __construct($name, $email, $password, $title)
   {
     parent::__construct($name, $email, $password);
@@ -100,3 +112,6 @@ class Employee extends User
 
 $employee1 = new Employee('John', 'johndoe@gmail.com', '123456', 'Manager');
 echo $employee1->getTitle();
+
+
+echo '<br/>---------------------------------------------------<br/>';
